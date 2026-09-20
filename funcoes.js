b4d2d27a814ac24f3aea145c0967a3f05300d2a9
THIAGO OLIVEIRA GONÇALVES SOARES <thioliver0804@gmail.com>
2026-09-20 16:08:03 -0300

    Add calcularMedia function and module exports

--- funcoes.js
+++ funcoes.js
@@ -1,3 +1,7 @@
+function calcularMedia(nota1, nota2) {
+ return (nota1 + nota2) / 2;
+}
+
 function verificarSituacao(media) {
  if (media >= 6) {
  return "Aprovado";
@@ -6,4 +10,11 @@ function verificarSituacao(media) {
  return "Recuperação";
  }
  return "Reprovado";
+}
+
+if (typeof module !== "undefined") {
+ module.exports = {
+ calcularMedia,
+ verificarSituacao
+ };
 }
